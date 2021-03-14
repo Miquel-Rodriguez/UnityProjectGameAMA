617f2e71a6403e93f539fc16284c4fbc871af745
Adrian <adrian.nunyez.7e3@itb.catr>
2021-03-14 19:10:31 +0100

    Hechos camara, movimiento por waypoints, y espera en los waypoints.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    Vector3 Angles;
+    public float sensitivityX;
+    public float sensitivityY;
+    void Update()
+    {
+        float rotationY = Input.GetAxis("Mouse Y") * sensitivityX;
+        float rotationX = Input.GetAxis("Mouse X") * sensitivityY;
+        if (rotationY > 0)
+            Angles = new Vector3(Mathf.MoveTowards(Angles.x, -80, rotationY), Angles.y + rotationX, 0);
+        else
+            Angles = new Vector3(Mathf.MoveTowards(Angles.x, 80, -rotationY), Angles.y + rotationX, 0);
+        transform.localEulerAngles = Angles;
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float speed;
+    [SerializeField] Transform target;
+    float maxSpeed;
+    void Start()
+    {
+        transform.LookAt(new Vector3(target.position.x, transform.position.y, target.position.z));
+        maxSpeed = speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(0, 0, speed * Time.deltaTime));
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Waypoint")
+        {
+            StartCoroutine(moveAndWait(other));
+            
+        }
+        
+    }
+
+    IEnumerator moveAndWait(Collider other)
+    {
+        yield return target = other.gameObject.GetComponent<WayPoint>().nextPoint;
+        speed = 0;
+        yield return new WaitForSeconds(5f);
+        if (speed == 0)
+        {
+            speed += maxSpeed;
+        }
+        
+        transform.LookAt(new Vector3(target.position.x, transform.position.y, target.position.z));
+       
+    }
+}

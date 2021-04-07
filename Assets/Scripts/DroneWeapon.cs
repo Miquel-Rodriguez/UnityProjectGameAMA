81870b1c82140e0362dd2eb935bbce261f826fbd
Aaron <aaron.martinez.7e3@itb.cat>
2021-04-07 03:42:17 +0200

    Enemigo dron con movimiento, disparos al player y particulas

---
+++ Assets/Scripts/DroneWeapon.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class DroneWeapon : Weapon
+{
+
+    public ParticleSystem particulaTiro;
+
+    public void Start()
+    {
+
+
+        transformRotation = gameObject.GetComponent<Transform>();
+        bulletsCharge = capacityCharger;
+
+        StartCoroutine(AddOneBullet());
+    }
+
+    public void Update()
+    {
+        if (bulletsCharge != 0)
+        {
+            if ( Time.time >= nextFireTime && !reloading)
+            {
+                InstanceShoot();
+                disparo();
+                LessBullet();
+            }
+        }
+
+        if (bulletsCharge == 0 && Time.time >= nextReload && Actualbullets != 0)
+        {
+            StartCoroutine(Reload());
+        }
+
+    }
+
+    private IEnumerator AddOneBullet()
+    {
+        while (true)
+        {
+            if (Actualbullets < 1)
+            {
+                Actualbullets=1;
+            }
+            yield return new WaitForSeconds(1);
+        }
+
+
+    }
+
+    private void disparo()
+    {
+        particulaTiro.Play();
+    }
+
+}
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+
+[CreateAssetMenu(fileName = "Weapon", menuName = "ScriptableObjects/Weapons", order = 1)]
+public class Weapon : MonoBehaviour
+{
+    [SerializeField]
+    protected string weaponName;
+    [SerializeField]
+    protected float reloadSpeed;
+    [SerializeField]
+    protected int capacityCharger;
+    [SerializeField]
+    protected float shootSpeed;
+    [SerializeField]
+    protected Transform shootPoint;
+
+    protected float angleShootDirection;
+
+    public bool reloading;
+    protected Transform transformRotation;
+    protected int bulletsCharge;
+
+    protected float nextFireTime = 0;
+    protected float nextReload = 0;
+    [SerializeField]
+    protected float speed;
+    [SerializeField]
+    protected GameObject bullet;
+    [SerializeField]
+    private int actualbullets;
+
+    [SerializeField]
+    protected float maxAngleVariationx;
+    [SerializeField]
+    protected float maxAngleVariationy;
+
+    public bool isAvailable;
+
+    public int Actualbullets { get => actualbullets; set => actualbullets = value; }
+
+    public virtual IEnumerator Reload()
+    {
+
+        reloading = true;
+        nextReload = Time.time + reloadSpeed;
+        yield return new WaitForSeconds(reloadSpeed);
+        if (actualbullets < capacityCharger)
+        {
+            bulletsCharge = actualbullets;
+            actualbullets = 0;
+        }
+        else
+        {
+            bulletsCharge = capacityCharger;
+            actualbullets -= bulletsCharge;
+        }
+
+        //mostrat bales totals 
+        reloading = false;
+    }
+
+    protected void InstanceShoot()
+    {
+        float variationx = Random.Range(-maxAngleVariationy, maxAngleVariationy);
+        float variationy = Random.Range(-maxAngleVariationx, maxAngleVariationx);
+
+
+        shootPoint.rotation *= Quaternion.Euler(variationx, variationy, 1);
+
+        GameObject go = Instantiate(bullet, shootPoint.position, Quaternion.Euler(transformRotation.rotation.eulerAngles.x + 90 + variationx, transformRotation.rotation.eulerAngles.y + variationy, 0));
+
+
+
+        go.GetComponent<Rigidbody>().velocity = shootPoint.forward * speed;
+        nextFireTime = Time.time + shootSpeed;
+        shootPoint.rotation = Quaternion.Euler(gameObject.transform.rotation.eulerAngles.x, gameObject.transform.rotation.eulerAngles.y, 1);
+    }
+
+    protected void LessBullet()
+    {
+        bulletsCharge--;
+    }
+
+    protected void AddBullets(int bulletsToAdd)
+    {
+        actualbullets += bulletsToAdd;
+    }
+
+
+
+}

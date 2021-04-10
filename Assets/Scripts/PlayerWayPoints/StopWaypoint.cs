70e0b805455ae3c6366f95195e6262c1406fa815
Adrian <adrian.nunyez.7e3@itb.catr>
2021-04-10 22:31:12 +0200

    s

---
+++ Assets/Scripts/PlayerWayPoints/StopWaypoint.cs
@@ -0,0 +1,22 @@
+﻿using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StopWaypoint : MonoBehaviour
+{
+    CinemachineDollyCart player;
+    
+    private void Start()
+    {
+        
+
+    }
+    private void Update()
+    {
+       
+
+    }
+
+   
+}

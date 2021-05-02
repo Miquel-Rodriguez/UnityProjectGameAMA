ee4dcb8609ff8678205de2926f6bdcaaf1f08f9d
Miquel <miquel.rodriguez.7e3@itb.cat>
2021-05-03 01:23:42 +0200

    123456789

--- Assets/Scripts/Weapons/DroneWeapon.cs
+++ Assets/Scripts/Weapons/DroneWeapon.cs
@@ -8,6 +8,16 @@ public class DroneWeapon : EnemyWeapon
     public ParticleSystem particulaTiro;
     public bool disparar = true;
 
+    RaycastHit hit;
+
+    [SerializeField]
+    Transform enemyEyes;
+
+    [SerializeField]
+    int visionRange;
+
+    [SerializeField]
+    Transform player;
     public void Start()
     {
         transformRotation = gameObject.GetComponent<Transform>();
@@ -18,13 +28,19 @@ public class DroneWeapon : EnemyWeapon
 
     public void Update()
     {
+        Debug.DrawRay(enemyEyes.position, enemyEyes.forward * visionRange, Color.red);
+        RotateTowards(player, enemyEyes);
         if (bulletsCharge != 0)
         {
-            if (Time.time >= nextFireTime && !reloading )
+            if (Time.time >= nextFireTime && !reloading && WatchingPlayer())
             {
+
+
                 InstanceShoot();
                 disparo();
                 LessBullet();
+
+
             }
         }
 
@@ -41,7 +57,11 @@ public class DroneWeapon : EnemyWeapon
         {
             if (Actualbullets < 1)
             {
-                Actualbullets=1;
+              
+
+
+
+                Actualbullets =1;
             }
             yield return new WaitForSeconds(1);
         }
@@ -52,6 +72,22 @@ public class DroneWeapon : EnemyWeapon
     public void disparo()
     {
         particulaTiro.Play();
+    }
+
+    private bool WatchingPlayer()
+    {
+
+        return Physics.Raycast(enemyEyes.position, enemyEyes.forward, out hit, visionRange) && hit.collider.CompareTag("Player");
+
+    }
+
+    public static void RotateTowards(Transform player, Transform npc, float speed = 2.0f)
+    {
+
+        Vector3 direction = (player.position - npc.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(direction);
+        npc.rotation = Quaternion.Slerp(npc.rotation, lookRotation, Time.deltaTime * speed);
+
     }
 
 }
